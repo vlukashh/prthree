9851637d3f941349b4c28e7524a1f2c6a8a0c310
vlukashh <ankalukashenko@gmail.ru>
2024-02-20 00:44:24 +0700

    сохранение данных

--- proj/src/main.js
+++ proj/src/main.js
@@ -11,4 +11,57 @@ new Vue({
         currentDateTime: '',
         enteredDeadline: '',
     },
+    mounted() {
+        this.loadTasksFromStorage(); // Загрузка задач из хранилища при монтировании компонента
+    },
+    watch: {
+        plannedTasks: {
+            handler() {
+                this.saveTasksToStorage(); // Сохранение задач в хранилище при изменении массива запланированных задач
+            },
+            deep: true,
+        },
+        inProgressTasks: {
+            handler() {
+                this.saveTasksToStorage(); // Сохранение задач в хранилище при изменении массива задач в процессе выполнения
+            },
+            deep: true,
+        },
+        testingTasks: {
+            handler() {
+                this.saveTasksToStorage(); // Сохранение задач в хранилище при изменении массива задач на тестировании
+            },
+            deep: true,
+        },
+        completedTasks: {
+            handler() {
+                this.saveTasksToStorage(); // Сохранение задач в хранилище при изменении массива выполненных задач
+            },
+            deep: true,
+        },
+    },
+    methods: {
+        saveTasksToStorage() {
+            const tasks = {
+                plannedTasks: this.plannedTasks,
+                inProgressTasks: this.inProgressTasks,
+                testingTasks: this.testingTasks,
+                completedTasks: this.completedTasks
+            };
+
+            localStorage.setItem('tasks', JSON.stringify(tasks)); // Сохранение задач в локальное хранилище
+        },
+        loadTasksFromStorage() {
+            const storedTasks = localStorage.getItem('tasks');
+
+            if (storedTasks) {
+                const tasks = JSON.parse(storedTasks);
+
+                this.plannedTasks = tasks.plannedTasks || [];
+                this.inProgressTasks = tasks.inProgressTasks || [];
+                this.testingTasks = tasks.testingTasks || [];
+                this.completedTasks = tasks.completedTasks || [];
+            }
+        }
+    }
 })
